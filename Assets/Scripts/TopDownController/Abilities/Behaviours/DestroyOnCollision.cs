e62462dc8ab422a2d4916d058624f64cb95df3d8
Francis Boisclair <francis.l.boisclair@gmail.com>
2020-04-25 15:28:21 -0400

    Add projectile collision + time to live

---
+++ Assets/Scripts/TopDownController/Abilities/Behaviours/DestroyOnCollision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+
+namespace FGSX.Abilities
+{
+    public class DestroyOnCollision : MonoBehaviour
+    {
+        public LayerMask m_Layers;
+
+        private void OnTriggerEnter(Collider other) {
+            if (((1 << other.gameObject.layer) & m_Layers) != 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TopDownController/Abilities/Behaviours/TimeToLive.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+public class TimeToLive : MonoBehaviour
+{
+    public float m_TimeAlive;
+    private float m_TimeToKill;
+
+
+    private void Start()
+    {
+        m_TimeToKill = Time.time + m_TimeAlive;
+    }
+    
+    private void Update()
+    {
+        if (Time.time >= m_TimeToKill)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
