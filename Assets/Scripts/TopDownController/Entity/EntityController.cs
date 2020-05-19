b142142fec6bd6a854c6eea7ec486accc1d33994
Francis Boisclair <francis.l.boisclair@gmail.com>
2020-05-19 19:44:48 -0400

    Add basic conveyor belt script

--- Assets/Scripts/TopDownController/Entity/EntityController.cs
+++ Assets/Scripts/TopDownController/Entity/EntityController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 
@@ -10,11 +11,22 @@ namespace FGSX.TopDownController.Entity
         [SerializeField] private float m_GravityScale = 1f;
 
         private CharacterController m_CharacterController;
+        private Dictionary<GameObject, Vector3> m_AdditionalMovementVelocities;
 
 
         private void Awake()
         {
             m_CharacterController = GetComponent<CharacterController>();
+        }
+
+        private void Start()
+        {
+            m_AdditionalMovementVelocities = new Dictionary<GameObject, Vector3>();
+        }
+
+        private void Update()
+        {
+            ApplyAdditionalMovementVelocity();
         }
 
 
@@ -38,9 +50,43 @@ namespace FGSX.TopDownController.Entity
 
         public void Roll(Vector3 direction)
         {
+            // Prevent from moving faster diagonally
             Vector3 rollDirection = (direction.sqrMagnitude > 1f) ? direction.normalized : direction;
             Vector3 rollVelocity = rollDirection * m_MoveSpeed;
             m_CharacterController.Move(rollVelocity * Time.deltaTime);
+        }
+
+        public void AddAdditionalMovementVelocity(GameObject gameObject, Vector3 velocity)
+        {
+            if (m_AdditionalMovementVelocities.ContainsKey(gameObject))
+            {
+                RemoveAdditionalMovementVelocity(gameObject);
+            }
+
+            m_AdditionalMovementVelocities.Add(gameObject, velocity);
+        }
+
+        public void RemoveAdditionalMovementVelocity(GameObject gameObject)
+        {
+            m_AdditionalMovementVelocities.Remove(gameObject);
+        }
+
+
+        private void ApplyAdditionalMovementVelocity()
+        {
+            m_CharacterController.Move(CalculateAdditionalMovementVelocity() * Time.deltaTime);
+        }
+
+        private Vector3 CalculateAdditionalMovementVelocity()
+        {
+            Vector3 velocity = Vector3.zero;
+
+            foreach (Vector3 additionalVelocity in m_AdditionalMovementVelocities.Values)
+            {
+                velocity += additionalVelocity;
+            }
+
+            return velocity;
         }
     }
 }
---
+++ Assets/Scripts/Traps/ConveyorBelt.cs
@@ -0,0 +1,30 @@
+﻿using FGSX.TopDownController.Entity;
+using UnityEngine;
+
+
+namespace FGSX.Traps
+{
+    public class ConveyorBelt : MonoBehaviour
+    {
+        [SerializeField] private float m_Speed;
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            EntityController entityController = other.GetComponent<EntityController>();
+            if (entityController)
+            {
+                entityController.AddAdditionalMovementVelocity(gameObject, transform.forward * m_Speed);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            EntityController entityController = other.GetComponent<EntityController>();
+            if (entityController)
+            {
+                entityController.RemoveAdditionalMovementVelocity(gameObject);
+            }
+        }
+    }
+}
